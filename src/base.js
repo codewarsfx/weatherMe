043d9a2707840fe07948f342b7be58e580c9074b
codewarsfx <chiderainnocent001@gmail.com>
2020-06-01 23:18:52 +0100

    search feauture complete

--- src/base.js
+++ src/base.js
@@ -2,4 +2,21 @@ export const elements = {
   inputElement: document.querySelector('.submit__input'),
   buttonELement: document.querySelector('.submit__button'),
   formELement: document.querySelector('.submit'),
+  mainElement: document.querySelector('.container'),
+  mainContent: document.querySelector('.main__content'),
+  asideDescription: document.querySelector('.weather__Description'),
+  loader: document.querySelector('.loader'),
+  asideElement: document.querySelector('aside'),
+  mainSpan: document.querySelector('.main__header span '),
+  weatherDescription: document.querySelector('.weather__Description'),
+};
+
+export const addLoader = () => {
+  // 1. change display property of display
+  elements.loader.style.display = 'block';
+};
+
+export const removeLoader = () => {
+  // 1. change display property of display
+  elements.loader.style.display = 'none';
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,49 @@
-import { elements } from '../base';
+import { elements, addLoader, removeLoader } from '../base';
 import Search from './models/search';
-import { readCurrentInput } from './views/searchView';
+import {
+  readCurrentInput,
+  clearInputField,
+  clearUI,
+  updateWeatherDetails,
+} from './views/searchView';
 
 //current state of data in app
 const state = {};
-
 /*
 
  SEARCH CONTROLLER
 
  */
 let SearchController = async () => {
+  //clear ui to prepare for new information
+  clearUI();
   //read value from input
-  const searchQuery = readCurrentInput();
-  //make a search instance
+  let searchQuery = readCurrentInput() || window.location.hash.replace('#', '');
+  //clear input field
+  clearInputField();
+  console.log(searchQuery);
   if (searchQuery) {
+    //add loader while user waits for data to be recieved from api
+    addLoader();
     state.search = new Search(searchQuery);
     //get weather data for the query location
-
     let data = await state.search.weatherSearch();
-    console.log(data);
+    //remove Loader
+    removeLoader();
+    // updateUI(data.description);
+    updateWeatherDetails(data);
   }
 };
 
 ////////////////////////////////////////
 
-//Event listener for search controller
+// Event listener for search controller
 elements.formELement.addEventListener('submit', (e) => {
   e.preventDefault();
+  SearchController();
+});
+
+window.addEventListener('hashchange', (e) => {
+  e.preventDefault;
   SearchController();
 });
